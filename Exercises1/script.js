1c152f4926c169724ac099304f0d451a4073f058
RaissaKouabenan <ruichoukouabenan@gmail.com>
2023-01-14 23:25:14 +0000

    first commit

---
+++ Exercises1/script.js
@@ -0,0 +1,24 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+/*question 1*/
+let fruit = fruits.shift("Banana");
+console.log(fruits);
+
+/*question 2*/
+
+let trie = fruits.sort();
+console.log(trie);
+
+/*question 3*/
+
+let ajout = fruits.push('Kiwi');
+console.log(fruits);
+
+/*question 4*/
+ let suppression = fruits.splice(0,1);
+ console.log(fruits);
+
+ /*question 5*/
+
+ let renverse = fruits.reverse();
+ console.log(renverse);
